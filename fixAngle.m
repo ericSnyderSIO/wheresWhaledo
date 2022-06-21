6478dc63c0d208d1e15f6461f6248796806534c9
akok <akok@ucsd.edu>
2022-06-21 14:38:17 -0700

    updated fixAngle

--- fixAngle.m
+++ fixAngle.m
@@ -2,6 +2,6 @@ function DET = fixAngle(DET)
 
 for i = 1:2
     DET{i}.DOA = -DET{i}.DOA;
-    DET{i}.Ang(:, 1) = atan2d(DET{i}.DOA(:,2), DET{i}.DOA(:,2));
+    DET{i}.Ang(:, 1) = atan2d(DET{i}.DOA(:,2), DET{i}.DOA(:,1));
     DET{i}.Ang(:, 2) = 180 - acosd(DET{i}.DOA(:,3)); % fix elevation
 end
