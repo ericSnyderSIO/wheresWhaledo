0e13987b41fe1c925a80deca2cb17ae18eb99c78
Lauren Baggett <81193849+laurenbaggett@users.noreply.github.com>
2023-06-06 14:21:50 -0700

    fix TDOA bug
    instead of saving all TDOAs, save only TDOAs from clicks received by both 4-ch arrays

--- loc3D_DOAintersect.m
+++ loc3D_DOAintersect.m
@@ -36,8 +36,8 @@ for wn = 1:length(colorNums) % iterate through each whale number
         t1 = DET{1}.TDet(I1); % times of detections on array 1
         t2 = DET{2}.TDet(I2); % times of detections on array 2
 
-        doa1 = DET{1}.DOA(I1, :);
-        doa2 = DET{2}.DOA(I2, :);
+        doa1 = DET{1}.DOA(I1, :); %% times of DOA on array 1
+        doa2 = DET{2}.DOA(I2, :); %% times of DOA on array 2
         
         i = 0;
         for i1 = 1:length(t1) % iterate through all detections on array 1
@@ -54,7 +54,9 @@ for wn = 1:length(colorNums) % iterate through each whale number
                 w(i, :) = mean([w1; w2]);
                 werr(i) = sqrt(sum((w1-w2).^2));
                 t1_used(i) = t1(i1);
+                t1_used_idx(i) = i1; % save for TDOA indexing later
                 t2_used(i) = t2(i2);
+                t2_used_idx(i) = i2; % save for TDOA indexing later
             end
         end
         whale{wn}.wloc = w;
@@ -65,8 +67,8 @@ for wn = 1:length(colorNums) % iterate through each whale number
         z = w(3,:) - abs(h0(3));
         whale{wn}.LatLonDepth = [lat; lon; z];
         whale{wn}.werr = werr;
-        whale{wn}.TDOA(:, 1:6) = DET{1}.TDOA(I1, :);
-        whale{wn}.TDOA(:, 7:12) = DET{2}.TDOA(I2, :);
+        whale{wn}.TDOA(:, 1:6) = DET{1}.TDOA(t1_used_idx, :); % only simultaneous TDOAs array 1
+        whale{wn}.TDOA(:, 7:12) = DET{2}.TDOA(t2_used_idx, :); % only simultaneous TDOAs array 2
         whale{wn}.I1 = I1;
         whale{wn}.I2 = I2;
         
